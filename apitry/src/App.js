87aded7d447431114c47fdbecb80558cfa10753a
jiwanchoi <wldhks303@naver.com>
2023-10-06 20:40:04 +0900

    회원가입, 로그인 api연동 마무리

--- apitry/src/App.js
+++ apitry/src/App.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import JoinPage from "./Components/JoinPage";
+import LoginPage from "./Components/LoginPage";
+
 function App() {
+  const [page,setPage]=useState(true);
+  const [info,setinfo]=useState("");
+  const handlePage = ()=>{
+    setPage((prevPage)=>{
+      return !prevPage
+    })
+  }
+  const getMyinfo = async ()=>{
+    const reqUrl = "https://api.mandarin.weniv.co.kr/user/myinfo";
+    const token = localStorage.getItem("token");
+    const res = await fetch(reqUrl,{
+        method:"GET",
+      headers:{
+        Authorization:`Bearer ${token}`
+      }
+    })
+    const json = await res.json();
+    console.log(json);
+    setinfo(JSON.stringify(json));
+  }
   return (
     <div>
-      hello world
+      <button type="button" onClick={getMyinfo}>내 정보 불러오기
+      </button>
+      {info}
+      {
+      page?
+      <LoginPage handlePage={handlePage}/>:<JoinPage handlePage={handlePage}/>
+      }
     </div>
   );
 }
